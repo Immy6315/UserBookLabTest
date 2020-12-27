82a41601c82806d448717fe2c531e231a2dc5a52
Imran Khan <Immy6315@gmail.com>
2020-12-28 03:26:42 +0530

    Update V1Routes.ts

--- src/app/routes/V1Routes.ts
+++ src/app/routes/V1Routes.ts
@@ -5,27 +5,11 @@ import LoginController from '../controllers/LoginController'
 
 @injectable()
 export default class V1Routes extends Routes {
-    // constructor(
-    //     private fileUploadMiddleware: FileUploadMiddleware
-    // ){
-    //     super();
-    //     console.log("Yes", this.fileUploadMiddleware)
-    // }
-    /**
-	 * IMPORTANT: User the '/' prefix before the path name
-	 * to avoid the 404 Error
-	 */
     basePath(): string {
         return '/v1'
     }
-
-    /**
-	 * IMPORTANT: User the '/' prefix before the path name
-	 * to avoid the 404 Error
-	 */
-    controllers(): IControllerRoute[]  {
-        // console.log(this.fileUploadMiddleware)
-
+	controllers(): IControllerRoute[]  {
+        
         return [{
             path: '/login',
             controller: container.get(LoginController),
